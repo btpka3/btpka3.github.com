1065a6124437fb4ac8884026953ce6aa1fe66d20
btpka3@163.com <btpka3@163.com>
2013-03-18 16:14:10 +0800

    d

--- java/spring/jdo/first-spring-jdo/src/main/java/me/test/first/spring/jdo/Main.java
+++ java/spring/jdo/first-spring-jdo/src/main/java/me/test/first/spring/jdo/Main.java
@@ -15,90 +15,93 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println();
-		System.out.println("==================== TEST START...");
-		try {
-			FileSystemXmlApplicationContext appCtxt = new FileSystemXmlApplicationContext(
-					"applicationContext.xml");
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("==================== TEST START...");
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    System.in));
+            FileSystemXmlApplicationContext appCtxt = new FileSystemXmlApplicationContext(
+                    "applicationContext.xml");
 
-			PersonDAO personDAO = (PersonDAO) appCtxt.getBean("personDAO");
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					System.in));
+            System.out.println("press enter to continue...");
+            br.readLine();
 
-			System.out.println();
-			System.out.println("==================== TEST 1 : list");
-			List<Person> list = personDAO.list();
-			System.out.println(list.size());
-			for (Person person : list) {
-				printPerson(person);
-			}
-			System.out.println("press enter to continue...");
-			br.readLine();
+            PersonDAO personDAO = (PersonDAO) appCtxt.getBean("personDAO");
 
-			System.out.println();
-			System.out.println("==================== TEST 2 : select");
-			Person p = personDAO.select("1");
-			System.out.println(p);
-			printPerson(p);
-			System.out.println("press enter to continue...");
-			br.readLine();
+            System.out.println();
+            System.out.println("==================== TEST 1 : list");
+            List<Person> list = personDAO.list();
+            System.out.println(list.size());
+            for (Person person : list) {
+                printPerson(person);
+            }
+            System.out.println("press enter to continue...");
+            br.readLine();
 
-			System.out.println();
-			System.out.println("==================== TEST 3 : insert");
-			p = new Person();
-			p.setAge(24);
-			p.setBirthday(DateUtils.parseDateStrictly("1984-04-04",
-					"yyyy-MM-dd"));
-			p.setImgData("444444".getBytes("UTF-16"));
-			p.setMale(true);
-			p.setName("zhao6");
-			personDAO.insert(p);
-			System.out.println("press enter to continue...");
-			br.readLine();
+            System.out.println();
+            System.out.println("==================== TEST 2 : select");
+            Person p = personDAO.select("1");
+            System.out.println(p);
+            printPerson(p);
+            System.out.println("press enter to continue...");
+            br.readLine();
 
-			System.out.println();
-			System.out.println("==================== TEST 4 : update");
-			Map<String, Object> person = new HashMap<String, Object>();
-			person.put("id", 2L);
-			person.put("birthday",
-					DateUtils.parseDateStrictly("2222-02-02", "yyyy-MM-dd"));
-			personDAO.update(person);
-			System.out.println("press enter to continue...");
-			br.readLine();
+            System.out.println();
+            System.out.println("==================== TEST 3 : insert");
+            p = new Person();
+            p.setAge(24);
+            p.setBirthday(DateUtils.parseDateStrictly("1984-04-04",
+                    "yyyy-MM-dd"));
+            p.setImgData("444444".getBytes("UTF-16"));
+            p.setMale(true);
+            p.setName("zhao6");
+            personDAO.insert(p);
+            System.out.println("press enter to continue...");
+            br.readLine();
 
-			System.out.println();
-			System.out.println("==================== TEST 4 : delete");
-			personDAO.delete(3L);
-			System.out.println("press enter to continue...");
-			br.readLine();
+            System.out.println();
+            System.out.println("==================== TEST 4 : update");
+            Map<String, Object> person = new HashMap<String, Object>();
+            person.put("id", 2L);
+            person.put("birthday",
+                    DateUtils.parseDateStrictly("2222-02-02", "yyyy-MM-dd"));
+            personDAO.update(person);
+            System.out.println("press enter to continue...");
+            br.readLine();
 
-		} catch (Throwable e) {
+            System.out.println();
+            System.out.println("==================== TEST 4 : delete");
+            personDAO.delete(3L);
+            System.out.println("press enter to continue...");
+            br.readLine();
 
-			e.printStackTrace();
-		}
-		System.out.println();
-		System.out.println("==================== TEST OVER.");
-	}
+        } catch (Throwable e) {
 
-	public static void printPerson(Person p) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		String bithStr = p.getBirthday() == null ? "" : sdf.format(p
-				.getBirthday());
-		String imgStr = null;
-		try {
-			if (p.getImgData() != null) {
-				imgStr = new String(p.getImgData(), "UTF-16");
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		System.out.printf(
-				"ID:%d, NAME:%s, AGE:%d, isMale?%s, Birthday:%s, IMG:%s\n",
-				p.getId(), p.getName(), p.getAge(), p.getMale(), bithStr,
-				imgStr);
-	}
+            e.printStackTrace();
+        }
+        System.out.println();
+        System.out.println("==================== TEST OVER.");
+    }
+
+    public static void printPerson(Person p) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String bithStr = p.getBirthday() == null ? "" : sdf.format(p
+                .getBirthday());
+        String imgStr = null;
+        try {
+            if (p.getImgData() != null) {
+                imgStr = new String(p.getImgData(), "UTF-16");
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.printf(
+                "ID:%d, NAME:%s, AGE:%d, isMale?%s, Birthday:%s, IMG:%s\n",
+                p.getId(), p.getName(), p.getAge(), p.getMale(), bithStr,
+                imgStr);
+    }
 }
