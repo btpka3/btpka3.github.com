fdbf97972c6656e2f8e7596df4d67c0967d7c2da
btpka3 <btpka3@163.com>
2013-03-19 00:34:43 +0800

    d

--- java/spring/first-spring-security/src/main/java/me/test/MyAuthority.java
+++ java/spring/first-spring-security/src/main/java/me/test/MyAuthority.java
@@ -5,10 +5,10 @@ import org.springframework.security.core.GrantedAuthority;
 public enum MyAuthority implements GrantedAuthority {
 
     /** 权限：读取所有预约记录 */
-    ROLE_READ_APPOINTMENT("ROLE_READ_APPOINTMENT"),
+    XROLE_READ_APPOINTMENT("XROLE_READ_APPOINTMENT"),
 
     /** 权限：读取所有员工信息 */
-    ROLE_READ_STAFF("ROLE_READ_STAFF");
+    XROLE_READ_STAFF("XROLE_READ_STAFF");
 
     /** 权限代码，该值可根据需要插入到【用户权限关联表】等表中的【权限】字段中 */
     private String authority = null;
