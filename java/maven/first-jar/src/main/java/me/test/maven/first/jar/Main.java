37917a4cdf78f0a7ff545280ab9d3dbd217e45de
btpka3 <btpka3@163.com>
2013-04-12 01:08:53 +0800

    d

--- java/maven/first-jar/src/main/java/me/test/maven/first/jar/Main.java
+++ java/maven/first-jar/src/main/java/me/test/maven/first/jar/Main.java
@@ -4,17 +4,20 @@ import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(Main.class);
         ClassPathResource rsc = new ClassPathResource("config.properties");
-        System.out.println(IOUtils.toString(rsc.getInputStream()));
+        logger.info(IOUtils.toString(rsc.getInputStream()));
 
         Main m = new Main();
-        System.out.println(m.add(1, 2));
-        System.out.println(m.repeat('A', 3));
+        logger.info(Integer.toString(m.add(1, 2)));
+        logger.info(m.repeat('A', 3));
     }
 
     public int add(int a, int b) {
