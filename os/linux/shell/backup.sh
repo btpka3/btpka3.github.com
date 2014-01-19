a652dfa6eb9ae333a340ff9e6ac4aaa5c4c425cc
btpka3 <btpka3@163.com>
2014-01-19 20:42:56 +0800

    Update backup.sh

--- os/linux/shell/backup.sh
+++ os/linux/shell/backup.sh
@@ -1,30 +1,64 @@
 #!/bin/bash
-LOG=/root/bak/bak.log
-BAK_DIR=/root/bak
-CUR_TIME="date +%Y-%m-%d.%H:%M:%S"
-CUR_BAK=$BAK_DIR/test.`$CUR_TIME`
 
-echo `$CUR_TIME` start back > $LOG
+. /etc/rc.d/init.d/functions
+DATE="date +%Y-%m-%d"
+TIME="date +%Y-%m-%d.%H:%M:%S"
 
-# get existed bakups
+# config
+BAK_DIR=/data/back/xxx
+CUR_BAK=$BAK_DIR/`$TIME`
+RUNNING_USER=postgres
+BACKUP_COUNT=2
+DB_NAME=xxx
+DB_USER=xxx
+
+if [ -x "/sbin/runuser" ]; then
+  SU="/sbin/runuser -m -s /bin/sh"
+else
+  SU="/bin/su -m -s /bin/sh"
+fi
+
+echo `$TIME` ============================ back up $DB_NAME
+
+# get existed backups (as Array)
 dirList=(`find $BAK_DIR -maxdepth 1 -type d  ! \( -path '*/\.*' -o -path $BAK_DIR \) | sort`)
-echo eee====${#dirList[@]}===$loopEnd
 
-# delete old backups
-let loopEnd=${#dirList[@]}-2
+# delete old backups, only keep the last one
+let loopEnd=${#dirList[@]}-$BACKUP_COUNT
 for i in `seq 0 $loopEnd`
 do
- echo `$CUR_TIME` remove backup ${dirList[i]} > $LOG
- echo delete=$i==${dirList[i]}
+  echo `$TIME` deleteing ${dirList[i]}
+  rm -fr ${dirList[i]} 2>&1  || {
+    exit $?
+  }
 done
 
 # bakup a new one
-echo `$CUR_TIME` backup a new one  ${CUR_BAK} > $LOG$
-mkdir $CUR_BAK
-if [[ $? -eq 0 ]]
-then
-  echo `${CUR_TIME}` backup success | tee > $LOG
-else
-  echo `$CUR_TIME` backup fail | tee > $LOG
-  rm -fr ${CUR_BAK}
-fi
+echo `$TIME` creating new backup dir
+$SU $RUNNING_USER -c "mkdir $CUR_BAK 2>&1" || {
+    rc=$?
+    rm -fr ${CUR_BAK} 2>&1 
+    exit $rc
+}
+
+T_NAME=(
+  T_1
+  T_2
+  T_3
+)
+echo ${T_NAME[0]}
+ echo ${T_NAME[1]}
+for T in ${T_NAME[@]} 
+do
+echo `$TIME` backing up $DB_NAME.$T
+$SU $RUNNING_USER -c "pg_dump -U ${DB_USER} -F t -b -E UTF8 -f ${CUR_BAK}/${DB_NAME}.$T.`$TIME`.tar -t $T ${DB_NAME} 2>&1" || {
+    rc=$?
+    rm -fr ${CUR_BAK} 2>&1 
+    exit $rc
+}
+done
+
+
+echo `$TIME` success.
+
+
