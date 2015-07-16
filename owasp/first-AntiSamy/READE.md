342da97e37941a04871f8a49e740fe5fadf87ee1
btpka3 <btpka3@163.com>
2015-07-16 21:46:51 +0800

    简单测试新的防止XSS的类库的处理

---
+++ owasp/first-AntiSamy/READE.md
@@ -0,0 +1,7 @@
+
+
+## ref
+
+* [OWASP Java HTML Sanitizer Project](https://www.owasp.org/index.php/OWASP_Java_HTML_Sanitizer_Project) : 可编程配置，推荐
+* [AntiSamy](https://www.owasp.org/index.php/Category:OWASP_AntiSamy_Project)， 只能通过XML文件进行配置
+* [Jsoup](http://jsoup.org/cookbook/cleaning-html/whitelist-sanitizer)：预防XSS的功能较弱，但解析HTML相当不错
---
+++ owasp/first-AntiSamy/src/main/java/me/test/Test123.java
@@ -0,0 +1,70 @@
+package me.test;
+
+import com.google.common.base.Throwables;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.owasp.html.Handler;
+import org.owasp.html.HtmlSanitizer;
+import org.owasp.html.HtmlStreamRenderer;
+import org.owasp.html.examples.EbayPolicyExample;
+import org.owasp.validator.html.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class Test123 {
+    static final String drityInput = "<script>alert(1)</script>"
+            + "<a href='#bb' data-one='1' class='aa bb' style='z-index:999; width:100%;       aa:bb;display:block;overflow:hidden;color:red;' onclick='xxx'>aa</a>"
+            + "<p align='center' style='z-index:999; width:100%; align:left;display:block;overflow:hidden;color:red;'>1111 &nbsp; </p>"
+            + "<iframe src='javascript:xxx'></iframe>"
+            + "<xxx>xxx</xxx>";
+
+    public static void main(String[] args) throws ScanException, PolicyException {
+        System.out.println("------------------------------------------ testJsoup()");
+        testJsoup();
+
+        System.out.println("------------------------------------------ testAntiSamy()");
+        testAntiSamy();
+
+        System.out.println("------------------------------------------ testHtmlSanitizer()");
+        testHtmlSanitizer();
+
+    }
+
+    public static void testJsoup() {
+        String safe = Jsoup.clean(drityInput, Whitelist.basic());
+        System.out.println(safe);
+    }
+
+    public static void testAntiSamy() throws PolicyException, ScanException {
+
+        InputStream prolicyIn = Test123.class.getResourceAsStream("antisamy-ebay-1.4.4.xml");
+        Policy policy = Policy.getInstance(prolicyIn);
+        AntiSamy as = new AntiSamy();
+
+        // 输出： <a href="#bb" style="width: 100.0%;">aa</a> xxx
+        CleanResults cr = as.scan(drityInput, policy, AntiSamy.SAX);
+        String cleanResult = cr.getCleanHTML();
+        System.out.println(cleanResult);
+    }
+
+    public static void testHtmlSanitizer() {
+        HtmlStreamRenderer renderer = HtmlStreamRenderer.create(
+                System.out,
+                // Receives notifications on a failure to write to the output.
+                new Handler<IOException>() {
+                    public void handle(IOException ex) {
+                        Throwables.propagate(ex);  // System.out suppresses IOExceptions
+                    }
+                },
+                // Our HTML parser is very lenient, but this receives notifications on
+                // truly bizarre inputs.
+                new Handler<String>() {
+                    public void handle(String x) {
+                        throw new AssertionError(x);
+                    }
+                });
+        HtmlSanitizer.sanitize(drityInput, EbayPolicyExample.POLICY_DEFINITION.apply(renderer));
+    }
+}
