8a5bef88919e4aaae0fc224bc4984ac1818a4974
btpka3 <btpka3@163.com>
2015-07-30 18:17:47 +0800

    加深对Mongo MapReduce 的理解

--- db/mapReduce.js
+++ db/mapReduce.js
@@ -39,6 +39,59 @@
  }
  */
 
+/* 测试 Array.prototype.sort()
+
+ var arr = [{age:10,name:'zhang3'}, {age:9,name:'li4'},{age:11,name:'wang5'}];
+ arr.sort(function(a,b){
+ if(!a && !b ){
+ return 0;
+ }
+ if(a && !b){
+ return 1;
+ }
+ if(!a && b){
+ return -1;
+ }
+ return a.age - b.age;
+ });
+
+
+ */
+
+var arr = [
+    {
+        "userName": "zhang3",
+        "age": 23,
+        "statUserTime": ISODate("2015-07-30T09:56:20.277Z")
+    }, {
+        "age": 22,
+        "province": "ZheJiang"
+    }
+];
+arr.sort(function (a, b) {
+    if (!a && !b) {
+        return 0;
+    }
+    if (a && !b) {
+        return 1;
+    }
+    if (!a && b) {
+        return -1;
+    }
+
+    if (!a.statUserTime && !b.statUserTime) {
+        return 0;
+    }
+    if (a.statUserTime && !b.statUserTime) {
+        return 1;
+    }
+    if (!a.statUserTime && b.statUserTime) {
+        return -1;
+    }
+    return a.statUserTime - b.statUserTime;
+});
+
+
 db.user.drop();
 db.order.drop();
 db.tmpUserOrder.drop();
@@ -70,42 +123,73 @@ db.order.insert({_id: 'o504', userId: 'u5', date: new Date(2015, 5, 15), status:
 /*
  // 期待结果
  tmpUserOrder: [
- { userId:"u3", userName:"zhang3", age:23, totalAmount: 600, orderIds : [o302,o303] },
- { userId:"u4", userName:"li4",    age:24, totalAmount: 300, orderIds : [o402,o403] },
- { userId:"u5", userName:"wang5",  age:25, totalAmount: 700, orderIds : [o503] }
+ { userId:"u3", userName:"zhang3", age:23, statUserTime: new Date(), totalAmount: 600, orderIds : [o302,o303], statOrderTime: new Date()},
+ { userId:"u4", userName:"li4",    age:24, statUserTime: new Date(), totalAmount: 300, orderIds : [o402,o403], statOrderTime: new Date()},
+ { userId:"u5", userName:"wang5",  age:25, statUserTime: new Date(), totalAmount: 700, orderIds : [o503],      statOrderTime: new Date()}
  ]
  */
 var mapUser = function () {
     var value = {
         userName: this.name,
-        age: this.age
+        age: this.age,
+        statUserTime: new Date()
     };
     emit(this._id, value);
 };
 
+
 // values ： map 函数的 相同 key 的 value 的集合 + 输出目标表中的已有记录
 // 具体情况是： u3 - valuse.lenght == 2, 其他为 1
-// reduce 方法会在map执行结果长度>1 时执行一次。Bug? 很不合理的一个设计
+// reduce 方法会在map执行结果长度>1 时执行一次。
 // reduce 方法在保存到数据库前，如果数据库中有同key记录，还会再执行一次（此时，values.lenght == 2）
 var reduceUser = function (key, values) {
     // 该打印语句需要到 mongod 所在服务器上查看其日志文件 
     // # tailf /data0/mongod/log/mongodb.log
-    printjson("--====================");
+    printjson("======================");
+    printjson(values);
+
+    // 排序，最新的放到最后
+    values = values.sort(function (a, b) {
+        if (!a && !b) {
+            return 0;
+        }
+        if (a && !b) {
+            return 1;
+        }
+        if (!a && b) {
+            return -1;
+        }
+
+        if (!a.statUserTime && !b.statUserTime) {
+            return 0;
+        }
+        if (a.statUserTime && !b.statUserTime) {
+            return 1;
+        }
+        if (!a.statUserTime && b.statUserTime) {
+            return -1;
+        }
+        return a.statUserTime - b.statUserTime;
+    });
     printjson(values);
 
     var result = {};
 
-    // FIXME：应当倒序遍历 values
-    values.forEach(function (value) {
+    for (var i = values.length - 1; i >= 0; i--) {
+        var value = values[i];
 
-        // 注意：数据库输出表中如果已有记录，则其是 values 中最后一个元素。
-        // 所以，如果处理不当，如本例所示，u3的age 应该是 23, 实际是 22
+        // 复制来自其他Collection的统计值
         for (var field in value) {
-            if (value.hasOwnProperty(field)) {
+            if (value.hasOwnProperty(field) && !result.hasOwnProperty(field)) {
                 result[field] = value[field];
             }
         }
-    });
+    }
+    // 这里要处理的值使用使用最新的
+    result.userName = values[values.length - 1].userName;
+    result.age = values[values.length - 1].age;
+    result.statUserTime = values[values.length - 1].statUserTime;
+
     return result;
 };
 
@@ -116,8 +200,9 @@ db.tmpUserOrder.find();
 
 var mapOrder = function () {
     var value = {
-        orderId: this._id,
-        amount: this.amount
+        totalAmount: this.amount,
+        orderIds: [this._id],
+        statOrderTime: new Date()
     };
     emit(this.userId, value);
 };
@@ -125,36 +210,49 @@ var reduceOrder = function (key, values) {
     printjson("********************");
     printjson(values);
 
-    var mapFileds = ['orderId', 'amount'];
-    var resultFields = ['totalAmount', 'orderIds'];
+    // 排序，最新的放到最后
+    values = values.sort(function (a, b) {
+        if (!a && !b) {
+            return 0;
+        }
+        if (a && !b) {
+            return 1;
+        }
+        if (!a && b) {
+            return -1;
+        }
 
-    // 新建 或 从数据库中获取原始值
-    var result = {
-        totalAmount: 0,
-        orderIds: []
-    };
+        if (!a.statOrderTime && !b.statOrderTime) {
+            return 0;
+        }
+        if (a.statOrderTime && !b.statOrderTime) {
+            return 1;
+        }
+        if (!a.statOrderTime && b.statOrderTime) {
+            return -1;
+        }
+        return a.statOrderTime - b.statOrderTime;
+    });
 
-    // 倒序遍历 values
+    printjson(values);
+
+
+
+    var result = {};
+
     for (var i = values.length - 1; i >= 0; i--) {
         var value = values[i];
 
-        // 计算总额
-        if (value.totalAmount) {
-            result.totalAmount += value.totalAmount;
-        }
-
-        // 记录 order ID
-        if (value.orderId) {
-            result.orderIds.push(value.orderId);
-        }
-
-        // 复制数据库中已有的值
+        // 复制来自其他Collection的统计值
         for (var field in value) {
-            if (value.hasOwnProperty(field) && mapFileds.lastIndexOf(field) < 0 && resultFields.lastIndexOf(field) < 0) {
+            if (value.hasOwnProperty(field) && !result.hasOwnProperty(field)) {
                 result[field] = value[field];
             }
         }
     }
+    // TODO statOrderTime -> statOrderVersion, statOrderVersion最大，且相同的结果合并，其他舍弃。需定义scope对象
+
+
     return result;
 };
 var beginDate = new Date(2015, 3, 1);
