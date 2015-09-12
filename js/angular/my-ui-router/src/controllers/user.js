d3aeadbcc1eb8237b3e996c121dc8c5ff8858c09
btpka3 <btpka3@163.com>
2015-09-12 16:39:54 +0800

    完善 ui-router 的demo

--- js/angular/my-ui-router/src/controllers/user.js
+++ js/angular/my-ui-router/src/controllers/user.js
@@ -10,20 +10,19 @@ app.config([
                     // 内容会填充到 id_1 下
                     templateUrl: 'views/user/index.root.html',
                     controller: ['$scope', '$http', function ($scope, $http) {
-
                     }]
                 },
                 'view2@': {
                     // 内容会填充到 id_2 下
-                    templateUrl: 'views/user/view2.root.html',
+                    templateUrl: 'views/user/view2.root.html'
                 },
                 '@user': {
                     // 内容会填充到 id_3 下
-                    templateUrl: 'views/user/index.user.html',
+                    templateUrl: 'views/user/index.user.html'
                 },
                 'view4@user': {
                     // 内容会填充到 id_4 下
-                    templateUrl: 'views/user/view4.user.html',
+                    templateUrl: 'views/user/view4.user.html'
                 }
 
             }
