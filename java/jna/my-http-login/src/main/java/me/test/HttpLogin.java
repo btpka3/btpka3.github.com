99f8e64886cea1f22c4ccae3043372af0f732964
btpka3 <btpka3@163.com>
2016-02-21 20:39:58 +0800

    update

--- java/jna/my-http-login/src/main/java/me/test/HttpLogin.java
+++ java/jna/my-http-login/src/main/java/me/test/HttpLogin.java
@@ -1,6 +1,11 @@
 package me.test;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.http.client.CookieStore;
@@ -18,11 +23,19 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.impl.cookie.DefaultCookieSpecProvider;
 import org.apache.http.impl.cookie.RFC6265CookieSpecProvider;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -30,14 +43,15 @@ import org.springframework.util.Assert;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * 需要先分析 http请求,在模拟http请求的数据,使用编程的方式进行登录.
- * 适合: Http请求中没有加密数据,没有复杂JS,ActiveX控件计算数据的情形.
+ * 需要先分析 http请求,在模拟http请求的数据,使用编程的方式进行登录. 适合:
+ * Http请求中没有加密数据,没有复杂JS,ActiveX控件计算数据的情形.
  * 
  * 参考:
  * http://hc.apache.org/httpcomponents-client-4.5.x/tutorial/html/statemgmt.html#d5e499
@@ -45,24 +59,97 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Configuration
 public class HttpLogin {
 
-    private static AnnotationConfigApplicationContext ctx = null;
-    private static String userName = "xx";
-    private static String pwd = "xx";
+    /** 分类 */
+    public static enum Category {
 
-    // 服务器返回的 userId
-    private static String userId = null;
+        /** 汽车房产 */
+        CAR_HOUSE("1001"),
+        /** 手机数码 */
+        MOBILE("1002"),
+        /** 电脑办公 */
+        PC("1003   "),
+        /** 电脑办公 */
+        APPLIANCE("1004");
+        // TODO 未完待续
+
+        private Category(String code) {
+            this.code = code;
+        }
+
+        private String code;
+
+        public String getCode() {
+            return code;
+        }
+    }
+
+    private static Logger logger = LoggerFactory.getLogger(HttpLogin.class);
+    private static AnnotationConfigApplicationContext ctx = null;
+
+    // 服务器返回的 数据
+    /*
+     *  {
+     *      userId : xxx,
+     *      category: {
+     *          ${Category} : {
+     *              1: [{         // 第一页的数据
+     *                  name    : "",       // 商品名称
+     *                  price   : 0,        // 商品价格
+     *                  itemId  : "",       // 商品ID （较短，下单用）
+     *                  itemCode: ""        // 商品代码
+     *              },{}]
+     *          }
+     *      
+     *      }
+     *  }
+     */
+    private static Map<String, Object> data = new LinkedHashMap<String, Object>();
+
+    static {
+
+        Map<Category, Map<Integer, Object>> categoryData = new LinkedHashMap<Category, Map<Integer, Object>>();
+
+        for (Category c : Category.values()) {
+            categoryData.put(c, new LinkedHashMap<Integer, Object>());
+        }
+
+        data.put("category", categoryData);
+    }
 
     public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+
+        String userName = "xx";
+        String pwd = "xx";
+        String toBuyItemId = "1161";
+
         ctx = new AnnotationConfigApplicationContext();
         ctx.register(HttpLogin.class);
         ctx.refresh();
 
+        // 测试1： 获取特定分类的商品列表
+        getItemsByCategory(Category.MOBILE, 1);
+        printItem(Category.MOBILE);
+
+        // 测试2： 登录
+        isLogined();
         index();
-        login();
-        getUserBuyList();
+        login(userName, pwd);
+        isLogined();
+
+        // 测试3： 下单：
+        // seq,     id,     code,      price, name
+        //   1,   1160,   342825,    7769.00, (第122期)佳能 EOS 70D 套机
+        //  29,   1161,   256386,     189.00, (第706期)丽芙 LifeVC 无线蓝牙天籁耳机
+        getItemRemainCount(toBuyItemId);
+        buy(toBuyItemId, 1);
+
+        // 测试4： 检查下单结果
+        // TODO
+        getUserBoughtList(0, (String) data.get("userId"), null, null, null);
+
     }
 
-    // 登录
+    // 主页
     @SuppressWarnings("unchecked")
     public static void index() {
         RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
@@ -73,14 +160,12 @@ public class HttpLogin {
 
         // 获取 cookie: "JSESSIONID"
         CookieStore cookieStore = ctx.getBean(CookieStore.class);
-        //System.out.println("====================" + resp.getHeaders().get("Set-Cookie"));
-        System.out.println(cookieStore.getCookies());
-
+        logger.debug("访问主页后，获得的cookie如下：" + cookieStore.getCookies());
     }
 
     // 登录
     @SuppressWarnings("unchecked")
-    public static void login() throws JsonParseException, JsonMappingException, IOException {
+    public static void login(String userName, String pwd) throws JsonParseException, JsonMappingException, IOException {
         RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
 
         String url = "http://www.qmduobao.com/login/login.html";
@@ -93,7 +178,10 @@ public class HttpLogin {
         // login
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Requested-With", "XMLHttpRequest");
-        ResponseEntity<String> resp = restTemplate.postForEntity(url, params, String.class);
+        HttpEntity<MultiValueMap<String, String>> reqEntity = new HttpEntity<MultiValueMap<String, String>>(params,
+                headers);
+
+        ResponseEntity<String> resp = restTemplate.postForEntity(url, reqEntity, String.class);
         String respStr = resp.getBody();
         Assert.isTrue(HttpStatus.OK == resp.getStatusCode(), "登录失败,http 状态码是 " + resp.getStatusCode());
         Assert.isTrue(!"userError".equals(respStr), "用户名不存在");
@@ -102,7 +190,9 @@ public class HttpLogin {
         // String -> JSON
         ObjectMapper objectMapper = ctx.getBean(ObjectMapper.class);
         Map<String, ?> jsonObj = objectMapper.readValue(respStr, Map.class);
-        System.out.println("登录成功,用户ID = " + jsonObj.get("userId"));
+        String userId = jsonObj.get("userId").toString();
+        data.put("userId", userId);
+        logger.debug("登录成功,用户ID = " + userId);
 
         CookieStore cookieStore = ctx.getBean(CookieStore.class);
 
@@ -136,50 +226,221 @@ public class HttpLogin {
         cookie.setAttribute(ClientCookie.DOMAIN_ATTR, ".qmduobao.com");
         cookieStore.addCookie(cookie);
 
-        System.out.println(cookieStore.getCookies());
+        logger.debug("手动追加部分cookie ： " + cookieStore.getCookies());
+    }
+
+    // 主页 : 最新揭晓 （Ajax）
+    @SuppressWarnings("unchecked")
+    public static void resultToBePublished() {
+        RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
+
+        String url = "http://www.qmduobao.com/lottery/lotteryproductutilList.html";
+        // TODO 
+    }
+
+    /**
+     * 特定商品分类下的商品ID
+     * 
+     * http://www.qmduobao.com/list/hot20/1002/p_1.html
+     * 
+     * @param category
+     * @param pageNo
+     *            页码，从1开始
+     * @throws UnsupportedEncodingException
+     */
+    @SuppressWarnings("unchecked")
+    public static void getItemsByCategory(
+            Category category,
+            int pageNo) throws UnsupportedEncodingException {
+
+        pageNo = pageNo <= 0 ? 1 : pageNo;
+        RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
+
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl("http://www.qmduobao.com/list/hot20/{categoryCode}/p_{pageNo}.html")
+                .build()
+                .expand(category.getCode(), pageNo)
+                .encode("UTF-8")
+                .toUri();
+
+        ResponseEntity<String> resp = restTemplate.getForEntity(uri, String.class);
+        Assert.isTrue(HttpStatus.OK == resp.getStatusCode());
+
+        Document doc = Jsoup.parse(resp.getBody());
+
+        Elements ulGoodsList = doc.select("#ulGoodsList > div[goodsid]");
+
+        List pageDataList = new ArrayList(ulGoodsList.size());
+
+        for (int i = 0; i < ulGoodsList.size(); i++) {
+
+            Element itemDom = ulGoodsList.get(i);
+            Map item = new LinkedHashMap();
+
+            item.put("name", itemDom.select("li.soon-list-name > a").text());
+            item.put("price", itemDom.select(".gray6").first().text().replace("价值：￥", ""));
+            item.put("itemId", itemDom.attr("goodsid"));
+            item.put("itemCode", itemDom.attr("codeid"));
+            pageDataList.add(item);
+        }
+
+        Map<Integer, Object> curCategoryData = (Map<Integer, Object>) ((Map<Category, Object>) data.get("category"))
+                .get(category);
+        curCategoryData.put(pageNo, pageDataList);
+
+        // 页次 : "1/2"
+        String pages = doc.select("#pagination > li.total_page > i").first().text();
+        int totalPage = Integer.parseInt(pages.split("/")[1]);
+
+        if (pageNo < totalPage) {
+            getItemsByCategory(category, pageNo + 1);
+        }
+    }
+
+    private static void printItem(Category category) {
+        logger.debug("目前获取的商品列表如下：（分类：{}）", category);
+        logger.debug(String.format("%4s, %6s, %8s, %10s, %s",
+                "seq",
+                "id",
+                "code",
+                "price",
+                "name"));
+
+        Map<Integer, Object> curCategoryData = (Map<Integer, Object>) ((Map<Category, Object>) data.get("category"))
+                .get(category);
+
+        int i = 0;
+        for (Object pageData : curCategoryData.values()) {
+            for (Object itemData : ((List) pageData)) {
+                logger.debug(String.format("%4s, %6s, %8s, %10s, %s",
+                        i,
+                        ((Map) itemData).get("itemId"),
+                        ((Map) itemData).get("itemCode"),
+                        ((Map) itemData).get("price"),
+                        ((Map) itemData).get("name")));
+                i++;
+            }
+        }
 
     }
 
-    // 获取夺宝列表
+    // 是否已经登录
+    // http://www.qmduobao.com/user/UserBuyList.html
     @SuppressWarnings("unchecked")
-    public static void getUserBuyList() throws JsonParseException, JsonMappingException, IOException {
+    public static boolean isLogined() throws JsonParseException, JsonMappingException, IOException {
+
         RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
 
-        String url = "http://www.qmduobao.com/user/UserBuyList.html";
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl("http://www.qmduobao.com/user/UserBuyList.html")
+                .build()
+                .encode("UTF-8")
+                .toUri();
 
-        // param
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-        params.add("userName", userName);
-        params.add("pwd", pwd);
+        ResponseEntity<String> resp = restTemplate.getForEntity(uri, String.class);
+
+        Assert.isTrue(HttpStatus.OK == resp.getStatusCode());
+        String respStr = resp.getBody();
+
+        // 包含则代表已经登录
+        boolean logined = respStr.contains("夺宝记录");
+
+        logger.debug("登录检查结果 = {}", logined ? "已登录" : "未登录");
+        return logined;
+
+    }
+
+    // 获取已登录用户的夺宝列表
+    @SuppressWarnings("unchecked")
+    public static void getUserBoughtList(
+            int pageNo, // 从0开始
+            String userId,
+            String selectTime,
+            String startDate,
+            String endDate)
+                    throws JsonParseException,
+                    JsonMappingException,
+                    IOException {
+
+        selectTime = selectTime == null ? "0" : selectTime;
+
+        RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
+
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl("http://www.qmduobao.com/user/userBuyListAjaxPage.action")
+                .queryParam("pageNo", pageNo < 0 ? 0 : pageNo)
+                .queryParam("userId", userId)
+                .queryParam("selectTime", selectTime)
+                .queryParam("startDate", startDate)
+                .queryParam("endDate", endDate)
+                .build()
+                .encode("UTF-8")
+                .toUri();
+
+        logger.debug("=================" + uri);
+        // get
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Requested-With", "XMLHttpRequest");
+        HttpEntity<Void> reqEntity = new HttpEntity<Void>(null, headers);
+        ResponseEntity<String> resp = restTemplate.exchange(uri, HttpMethod.GET, reqEntity, String.class);
+        Assert.isTrue(HttpStatus.OK == resp.getStatusCode());
+        String respStr = resp.getBody();
+        logger.debug("@@@\n" + respStr);
+
+    }
+
+    // 查询特定宝贝的剩余数量（Ajax）
+    // http://www.qmduobao.com/list/isStatus.action?id=1137
+    @SuppressWarnings("unchecked")
+    public static int getItemRemainCount(String itemId) throws JsonParseException, JsonMappingException, IOException {
+        RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
+
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl("http://www.qmduobao.com/list/isStatus.action")
+                .queryParam("id", itemId)
+                .build()
+                .encode("UTF-8")
+                .toUri();
 
         // get
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Requested-With", "XMLHttpRequest");
-        ResponseEntity<String> resp = restTemplate.getForEntity(url, String.class);
+        HttpEntity<Void> reqEntity = new HttpEntity<Void>(null, headers);
+        ResponseEntity<String> resp = restTemplate.exchange(uri, HttpMethod.GET, reqEntity, String.class);
+
+        Assert.isTrue(HttpStatus.OK == resp.getStatusCode());
         String respStr = resp.getBody();
-        Assert.isTrue(HttpStatus.OK == resp.getStatusCode(), "ERROR, http 状态码是 " + resp.getStatusCode());
+        int cremainCount = Integer.parseInt(respStr);
+        logger.debug("商品 {} 剩余份数： {} ", itemId, cremainCount);
+        return cremainCount;
+    }
 
-        // String -> HTML
-        if (respStr.contains("夺宝记录")) {
-            System.out.println("=====OK, 获取成功");
-            System.out.println(respStr);
-        } else {
-            System.out.println("=====ERROR, 获取失败");
-        }
+    // 主页：立即全民夺宝（Ajax）
+    // http://www.qmduobao.com/mycart/getShopResult.action?id=1137&moneyCount=1
+    @SuppressWarnings("unchecked")
+    public static void buy(
+            String itemId,
+            int moneyCount) throws JsonParseException, JsonMappingException, IOException {
 
-        //System.out.println(respStr);
+        RestTemplate restTemplate = ctx.getBean(RestTemplate.class);
 
-        // 说明: 如果解析html的话,请使用 JSOUP
-        //      该页面,浏览器上查看的 "夺宝记录" 是通过AJax请求获取的,URL如下(注意:不需要登录的额)
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl("http://www.qmduobao.com/mycart/getShopResult.action?")
+                .queryParam("id", itemId)
+                .queryParam("moneyCount", moneyCount <= 0 ? 1 : moneyCount)
+                .build()
+                .encode("UTF-8")
+                .toUri();
 
-        System.out.println("===================================================");
-        url = "http://www.qmduobao.com/user/userBuyListAjaxPage.action?pageNo=0&userId="
-                + userId
-                + "&selectTime=0&startDate=&endDate=";
-        resp = restTemplate.getForEntity(url, String.class);
-        respStr = resp.getBody();
-        System.out.println(respStr);
+        // get
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Requested-With", "XMLHttpRequest");
+        HttpEntity<Void> reqEntity = new HttpEntity<Void>(null, headers);
+        ResponseEntity<String> resp = restTemplate.exchange(uri, HttpMethod.GET, reqEntity, String.class);
+        Assert.isTrue(HttpStatus.OK == resp.getStatusCode());
 
+        String respStr = resp.getBody();
+        logger.debug("对商品 {} 下单结果为 ： {} ", itemId, respStr);
     }
 
     // ------------------------------------------------------- Spring Bean 配置区域
@@ -202,22 +463,15 @@ public class HttpLogin {
         PublicSuffixMatcher publicSuffixMatcher = PublicSuffixMatcherLoader.getDefault();
 
         Registry<CookieSpecProvider> r = RegistryBuilder.<CookieSpecProvider> create()
-                .register(CookieSpecs.DEFAULT,
-                        new DefaultCookieSpecProvider(publicSuffixMatcher))
-                .register(CookieSpecs.STANDARD,
-                        new RFC6265CookieSpecProvider(publicSuffixMatcher))
-                .build();
+                .register(CookieSpecs.DEFAULT, new DefaultCookieSpecProvider(publicSuffixMatcher))
+                .register(CookieSpecs.STANDARD, new RFC6265CookieSpecProvider(publicSuffixMatcher)).build();
 
-        RequestConfig globalConfig = RequestConfig.custom()
-                .setCookieSpec(CookieSpecs.DEFAULT)
-                .build();
+        RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.DEFAULT).build();
 
         return HttpClients.custom()
                 .setUserAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0")
-                .setDefaultCookieSpecRegistry(r)
-                .setDefaultCookieStore(cookieStore())
-                .setDefaultRequestConfig(globalConfig)
-                .build();
+                .setDefaultCookieSpecRegistry(r).setDefaultCookieStore(cookieStore())
+                .setDefaultRequestConfig(globalConfig).build();
     }
 
     @Bean
