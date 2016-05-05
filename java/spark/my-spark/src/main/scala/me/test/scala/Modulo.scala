6706ed31c8b0f1a9a1865c70ca867ea0b0c9abf6
btpka3 <btpka3@163.com>
2016-05-05 19:27:16 +0800

    add a

--- java/spark/my-spark/src/main/scala/me/test/scala/Modulo.scala
+++ java/spark/my-spark/src/main/scala/me/test/scala/Modulo.scala
@@ -1,24 +1,65 @@
 package me.test.scala
 
 import breeze.linalg.{DenseMatrix, Matrix}
-import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.ListBuffer
 
 /**
   * Created by zll on 16-5-4.
   */
 object Modulo {
   def main(args: Array[String]): Unit = {
+    //println(mapToMatrix(Array("01010", "10100", "01101", "11101", "00111", "01011")))
+    //    val arr = Array("XX.,.XX", "X,X,X,X", "X.XX,XXX.,.X..,.XX.", "XXX,.X.", "XXX..,X.XXX", "..X.,XXXX", ".X,XX,XX,X.", ".XXX,.XX.,..X.,XXX.,..X.", "...X,...X,XXXX", "..X,XXX", "..XX.,XXXXX,XX..X", "X..,X..,XXX,.X.")
+    //    for (a <- arr) {
+    //      println("------------------------------------")
+    //      println(pieceToMatrix(a))
+    //    }
 
 
-    // http://stackoverflow.com/questions/31064753/how-pass-scala-array-into-scala-vararg-method
-    var strArr = Array("01010", "10100", "01101", "11101", "00111", "01011")
+    val nums = 1 :: (2 :: (3 :: (4 :: Nil)))
+    println(nums)
+    println("111" :: "222" :: "333" :: Nil)
+    var z: List[String] = List()
+    //    z += "ccc"
+    println(z)
+    var z1 = new ListBuffer[String]()
+    z1 += "ccc"
+    z1 += "ddd"
+    println(z1)
 
-    val a = strArr.map(str => str.map(x => x - 48)) // x => x.toString.toInt
-    println(DenseMatrix(a: _*))
+
+
+
+    val fruit1 = "apples" :: ("oranges" :: ("pears" :: Nil))
+    val fruit2 = "mangoes" :: ("banana" :: Nil)
+
+    var ab = fruit1 ::: fruit2
+    println(ab.::("ccc").::("ddd"))
+
 
   }
+
+
+  def mapToMatrix(strArr: Array[String]): Matrix[Int] = {
+    // http://stackoverflow.com/questions/31064753/how-pass-scala-array-into-scala-vararg-method
+    //var strArr = Array("01010", "10100", "01101", "11101", "00111", "01011")
+    val a = strArr.map(str => str.map(x => x - 48)) // x => x.toString.toInt
+    DenseMatrix(a: _*)
+  }
+
+  def pieceToMatrix(str: String): Matrix[Int] = {
+    val a = str.split(",").map(str => str.map(x => if ('X' == x) 1 else 0))
+    DenseMatrix(a: _*)
+  }
+
+  def calcPiecePos(map: Matrix[Int], piece: Matrix[Int]): Array[(Int, Int)] = {
+    val arr = new Array[(Int, Int)](0)
+    arr
+  }
+
 
   def main1(args: Array[String]): Unit = {
     val conf = new SparkConf()
@@ -32,15 +73,8 @@ object Modulo {
       Array(p + 100, p + 200)
     }
     println(d.collect().toList)
-
-
   }
 
-  //  def toMatrix(strArr: Array[String]): Matrix[Int] = {
-  //
-  //    strArr.map(str => str.map(x => x - 48)) // x => x.toString.toInt
-  //
-  //  }
 
   def main2(args: Array[String]): Unit = {
 
--- java/spark/my-spark/src/test/scala/me/test/scala/ModuloTest.scala
+++ java/spark/my-spark/src/test/scala/me/test/scala/ModuloTest.scala
@@ -1,12 +1,25 @@
 package me.test.scala
-
+//import breeze.linalg._
 import org.junit.Assert._
 import org.junit.Test
-import org.junit.Before
 
 class ModuloTest {
 
   @Test def test001() {
     assertTrue(1 == 1.0)
+  }
+
+  @Test def testMapToMatrix() {
+//
+//    val m1 = Modulo.mapToMatrix(Array("100", "010", "001"))
+//    val m2 = DenseMatrix(
+//      Array(1, 0, 0, 0, 0),
+//      Array(0, 1, 0, 0, 0),
+//      Array(0, 0, 1, 0, 0),
+//      Array(0, 0, 0, 1, 0),
+//      Array(0, 0, 0, 0, 1)
+//    )
+//
+//    assertTrue(m1 == m2)
   }
 }
