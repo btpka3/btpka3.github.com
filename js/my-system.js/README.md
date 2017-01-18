ceec1079eaa5c9b866d101e4d1fbf7b9db813979
btpka3 <btpka3@163.com>
2017-01-18 17:20:43 +0800

    update

--- js/my-system.js/README.md
+++ js/my-system.js/README.md
@@ -13,6 +13,12 @@ traceur --out .tmp/load-esm.html.aaa.js --moudle load-esm.html.aaa.js
 npm install jspm -g
 npm install --save-dev jspm 
 jspm init -p    # 其中有一个步骤用以选择用哪种 ES6 transpiler.
+
+# 打包，需要明确使用脚本去 import
+# FIXME 如果生成的css在子目录下，目前无论怎么设置 separateCSS, buildCSS, baseURL, baseURL 都出问题
+jspm bundle --minify angular1-app.js bundle.js 
+
+# FIXME 如何线上环境打包时，只打包所需的？还是依靠CDN 按需回源站？
 ```
 
 ## 示例
--- js/my-system.js/angular1-app.js
+++ js/my-system.js/angular1-app.js
@@ -1,9 +1,12 @@
 import angular from "angular";
 import "angular-material";
+import "bootstrap";
+import "bootstrap/css/bootstrap.css!"
+import "./angular1.css!"  // FIMXE: 为何通过 meta:{'*.css': {loader: 'css'}} 仍然不起作用？
 import IndexController from "./angular1-IndexController.js";
 
 
 var app = angular.module("app", ['ngMaterial']);
 app.controller("IndexController", IndexController);
-console.log("-------1111");
+console.log("-------1112");
 export default  app;
