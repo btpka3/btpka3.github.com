a174ba727677bc4aa490cbdb6ac5f2e4d8fc9c2b
般若 <btpka3@163.com>
2017-12-11 19:57:26 +0800

    update

--- js/angular/my-ng4-ionic3/README.md
+++ js/angular/my-ng4-ionic3/README.md
@@ -1,2 +1,12 @@
 # 目的
 尝试将 ionic3 用在纯网站应用中
+
+
+```bash
+ng new \
+  --routing true \
+  --style scss \
+  --skip-install \
+  xxx
+
+```
--- js/angular/my-ng4-ionic3/src/app/app.module.ts
+++ js/angular/my-ng4-ionic3/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicModule,NavController } from 'ionic-angular';
 
 import { AppComponent } from './app.component';
 import { HomePage } from '../pages/home.page';
