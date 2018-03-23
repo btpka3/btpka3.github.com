c3b963ab9285548fac5a0ed7870f30595c469d45
btpka3 <btpka3@163.com>
2018-03-23 16:56:02 +0800

    add file upload limit

--- java/spring/first-spring-boot/first-spring-boot-jersey/src/main/groovy/me/test/first/spring/boot/jersey/resource/file/FileResource.groovy
+++ java/spring/first-spring-boot/first-spring-boot-jersey/src/main/groovy/me/test/first/spring/boot/jersey/resource/file/FileResource.groovy
@@ -4,24 +4,15 @@ import groovy.util.logging.Slf4j
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
+import org.apache.commons.io.IOUtils
+import org.apache.commons.io.input.BoundedInputStream
+import org.glassfish.jersey.media.multipart.FormDataParam
 import org.springframework.stereotype.Component
 
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
-/*
- 上传文件限制大小：
-  @FormDataParam("file") InpuptStream inStream
-  然后用 Commons-io, BoundedInputStream 包装限定一下。
-
-Spring配置文件配置项: spring.http.multipart.max-file-size
-只对 Spring 的 DispatcherServlet 有用。
-    MultipartProperties
-    -> MultipartAutoConfiguration
-    -> MultipartConfigElement
-    -> DispatcherServletRegistrationConfiguration
-*/
 // multipart/form-data
 // application/octet-stream
 @javax.inject.Singleton
@@ -37,6 +28,42 @@ class FileResource {
 
     FileResource() {
         println "================= new ${this.class.name}() : @ " + new Date()
+
+
+    }
+
+/*
+ 上传文件限制大小：
+  @FormDataParam("file") InputStream inputStream
+  然后用 Commons-io, BoundedInputStream 包装限定一下。
+  注意：
+    BoundedInputStream 在超过最大大小时，不会抛出异常，而是直接关闭流。
+    因此，可以 配合 CountingInputStream，或通过别的方式 知道已读取流的字节数。
+    而上传大小应当至少多一个字节，以便判断到底是超了，还是刚刚好。
+
+Spring配置文件配置项: spring.http.multipart.max-file-size
+只对 Spring 的 DispatcherServlet 有用。
+    MultipartProperties
+    -> MultipartAutoConfiguration
+    -> MultipartConfigElement
+    -> DispatcherServletRegistrationConfiguration
+*/
+
+    private void limitSize(
+            @FormDataParam("file")
+                    InputStream inputStream
+    ) {
+        long maxSize = 1024 * 1024 * 10;
+        File tmpFile = new File("/tmp/randomFile");
+
+        long copedSize = IOUtils.copy(
+                new BoundedInputStream(inputStream, maxSize + 1),
+                new FileOutputStream(tmpFile)
+        )
+        if (copedSize > maxSize) {
+            tmpFile.delete();
+            throw new RuntimeException("exceed max size: " + maxSize);
+        }
     }
 
     // 通过表单上传文件
