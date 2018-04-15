9333398f69e822f3e72a7e9e560f55e379a83d82
btpka3 <btpka3@163.com>
2018-04-15 16:31:58 +0800

    first vertx: http server

---
+++ java/vertx/first-vertx/README.md
@@ -0,0 +1,20 @@
+
+## build
+
+```bash
+# brew install vert.x
+./gradlew run
+./gradlew shadowJar
+```
+
+## 参考
+- [vert.x](https://vertx.io/docs/vertx-core/java/#_writing_http_servers_and_clients)
+- [用惯了高大上SpringBoot不妨试试小清新Vert.x](https://segmentfault.com/a/1190000011763020)
+- [vertx-examples/gradle-simplest](https://github.com/vert-x3/vertx-examples/tree/master/gradle-simplest)
+
+## FIXME
+- 如何与 Spring 框架融合？
+- 如何 做到 filter 机制，是否有类似 Servlet 框架
+- 如何 MVC？
+- vs Spring Webflux ?
+- Jax-RS ?
---
+++ java/vertx/first-vertx/src/main/java/com/github/btpka3/first/vertx/Main.java
@@ -0,0 +1,19 @@
+package com.github.btpka3.first.vertx;
+
+import io.vertx.core.Vertx;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an HTTP server which simply returns "Hello World!" to each request.
+        Vertx.vertx()
+                .createHttpServer()
+                .requestHandler(req -> req.response().end("Hello World!"))
+                .listen(8080, handler -> {
+                    if (handler.succeeded()) {
+                        System.out.println("http://localhost:8080/");
+                    } else {
+                        System.err.println("Failed to listen on port 8080");
+                    }
+                });
+    }
+}
