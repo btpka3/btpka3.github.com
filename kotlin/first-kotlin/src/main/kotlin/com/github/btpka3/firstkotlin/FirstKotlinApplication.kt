fa20ec4497545121c29e633b55701061a05270e5
btpka3 <btpka3@163.com>
2018-11-08 10:49:40 +0800

    init kotlin starter

---
+++ kotlin/first-kotlin/src/main/kotlin/com/github/btpka3/firstkotlin/FirstKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.github.btpka3.firstkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstKotlinApplication>(*args)
+}
---
+++ kotlin/first-kotlin/src/test/kotlin/com/github/btpka3/firstkotlin/FirstKotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.github.btpka3.firstkotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FirstKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
