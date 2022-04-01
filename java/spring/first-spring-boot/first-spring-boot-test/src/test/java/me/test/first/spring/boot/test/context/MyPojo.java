8e399cba40d164adaa14190db1915dd11ed3b093
btpka3 <btpka3@163.com>
2022-04-01 21:31:42 +0800

    update

--- java/spring/first-spring-boot/first-spring-boot-test/src/test/java/me/test/first/spring/boot/test/context/MyPojo.java
+++ java/spring/first-spring-boot/first-spring-boot-test/src/test/java/me/test/first/spring/boot/test/context/MyPojo.java
@@ -26,4 +26,8 @@ public class MyPojo {
 
     private String name;
     private MyPojo myPojo;
+
+    public String getNameStr() {
+        return this.name + "," + (myPojo != null ? myPojo.getName() : "");
+    }
 }
