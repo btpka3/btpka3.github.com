45d20d8864fcf9fa58a4b81eeb15d5118ab6da2e
btpka3 <btpka3@163.com>
2024-06-21 17:46:30 +0800

    udpate

--- java/jdk/TestJDK/hello/src/test/java/me/test/jdk/java/lang/StringTest01.java
+++ java/jdk/TestJDK/hello/src/test/java/me/test/jdk/java/lang/StringTest01.java
@@ -1,5 +1,9 @@
 package me.test.jdk.java.lang;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  *
  */
@@ -31,4 +35,59 @@ public class StringTest01 {
         }
         System.out.println(text);
     }
+
+    @Test
+    public void threeDoubleQuoteMarks01() {
+        String str = """
+                aaa   
+                """;
+        // 删除了头部空白，行尾空白，但保留了换行符
+        assertEquals("aaa\n", str);
+    }
+
+    @Test
+    public void threeDoubleQuoteMarks02() {
+        String str = """
+                aaa   
+                bbb   """;
+        assertEquals("aaa\nbbb", str);
+    }
+
+    /**
+     * 如果行尾通过 "\" escape 换行符，则该行的行尾空白将保留。
+     */
+    @Test
+    public void threeDoubleQuoteMarks03() {
+        String str = """
+                aaa   \
+                bbb   """;
+        assertEquals("aaa   bbb", str);
+    }
+
+    /**
+     * Idea Intellj 中可以通过注释 "// language=<language_ID>" 启用java text block 进行语法着色和校验。
+     *
+     * @see <a href="https://www.jetbrains.com/help/idea/using-language-injections.html#use-language-injection-comments">IDEA Intellij : Language injections</a></a>
+     */
+    @Test
+    public void threeDoubleQuoteMarks04() {
+        // language=JSON
+        String str = """
+                {
+                  "a": "aaa
+                }
+                """;
+        // language=JSON
+        String str1="{\"a\":\"aaa}";
+    }
+
+
+
+
 }
+
+
+
+
+
+
