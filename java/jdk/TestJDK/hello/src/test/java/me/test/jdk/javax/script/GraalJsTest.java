b30027e2ddf690694c06d05f9c78f4ced20d3d79
btpka3 <btpka3@163.com>
2025-02-28 02:20:04 +0800

    test ScriptEngine:groovy

--- java/jdk/TestJDK/hello/src/test/java/me/test/jdk/javax/script/GraalJsTest.java
+++ java/jdk/TestJDK/hello/src/test/java/me/test/jdk/javax/script/GraalJsTest.java
@@ -3,13 +3,11 @@ package me.test.jdk.javax.script;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineFactory;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 /**
  * @see <a href="https://www.graalvm.org/latest/reference-manual/js/ScriptEngine/">GraalVM:Js:ScriptEngine Implementation</a>
@@ -23,7 +21,7 @@ public class GraalJsTest {
         List<ScriptEngineFactory> factories = manager.getEngineFactories();
         for (ScriptEngineFactory f : factories) {
             System.out.println("egine name:" + f.getEngineName());
-             System.out.println("engine version:" + f.getEngineVersion());
+            System.out.println("engine version:" + f.getEngineVersion());
             System.out.println("language name:" + f.getLanguageName());
             System.out.println("language version:" + f.getLanguageVersion());
             System.out.println("names:" + f.getNames());
@@ -33,7 +31,12 @@ public class GraalJsTest {
         }
         System.out.println("-----------------------------------------------Done");
 
-        ScriptEngine engine = manager.getEngineByName("graal-js");
+        //ScriptEngine engine = manager.getEngineByName("graal-js");
+        ScriptEngine engine = manager.getEngineByName("js");
+        Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+        bindings.put("polyglot.js.allowHostAccess", true);
+        bindings.put("polyglot.js.allowHostClassLookup", (Predicate<String>) s -> true);
+
         engine.put("a", 4);
         engine.put("b", 6);
         Object maxNum = engine.eval("function max_num(a,b){return (a>b)?a:b;}max_num(a,b);");
@@ -44,11 +47,8 @@ public class GraalJsTest {
         m.put("c", 10);
         engine.put("m", m);
 
-        Map u = new HashMap();
-        u.put("r", "rrr");
-        engine.put("RSAUtils", u);
-
-        engine.eval("var x= max_num(a,m['c']);");
+        Object obj = engine.eval("var x= max_num(a,m.get('c'));");
+        System.out.println("obj:" + obj);
         System.out.println("max_num:" + engine.get("x"));
         Assertions.assertEquals(10, engine.get("x"));
     }
---
+++ java/jdk/TestJDK/hello/src/test/java/me/test/jdk/javax/script/GroovyTest.java
@@ -0,0 +1,26 @@
+package me.test.jdk.javax.script;
+
+import org.junit.jupiter.api.Test;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * @author dangqian.zll
+ * @date 2025/2/28
+ */
+public class GroovyTest {
+
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void test01() throws ScriptException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("groovy");
+        engine.put("a", 4);
+        engine.put("b", 6);
+        Object maxNum = engine.eval("def max_num(int a,int b){return (a>b)?a:b;}; return max_num(a,b);");
+        System.out.println("max_num:" + maxNum + ", (class = " + maxNum.getClass() + ")");
+    }
+}
