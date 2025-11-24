019b31ff4b40b565c74534b6459331cecd578764
btpka3 <btpka3@163.com>
2025-11-24 21:47:35 +0800

    update

--- java/maven/first-spotless/src/main/java/me/test/first/spotless/A.java
+++ java/maven/first-spotless/src/main/java/me/test/first/spotless/A.java
@@ -1,22 +1,23 @@
 package me.test.first.spotless;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.Arrays;
 import java.util.function.Function;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  */
 public class A {
 
     public void code1() {
-        Person p = Person.builder().name("zhang3").age(11).gender("male").build();
+Person p = Person.builder().name("zhang3")
+        .age(11)
+            .gender("male")
+        .build();
         System.out.println("person = " + p);
     }
 
     public void code2() {
-        Person p = Person.builder()
-                .name("zhang3")
+Person p = Person.builder().name("zhang3")
                 .age(11)
                 .gender("male")
                 .job("coder")
@@ -24,27 +25,25 @@ public class A {
         System.out.println("person = " + p);
     }
 
-    public String handle(String s , Function<String,String> fn){
+    public String handle(String s, Function<String, String> fn) {
         return fn.apply(s);
     }
 
-    public void code3(){
+    public void code3() {
 
-        Arrays.asList("aaa","bbb",  "ccc").stream()
-                .filter(StringUtils::isNotBlank) .map(s->s  + "123")
-                .map(s -> handle(s,
-                        str->{
-
-                            return s+"aaa";
-                            }
-                ))
-                .forEach(s->{
-                    if(s.length()>100){
+        Arrays.asList("aaa", "bbb", "ccc").stream()
+                .filter(StringUtils::isNotBlank)
+                .map(s -> s + "123")
+                .map(s -> handle(s, str -> {
+                    return s + "aaa";
+                }))
+                .forEach(s -> {
+                    if (s.length() > 100) {
                         System.out.println(s);
-                    }else if(s.length()>10){
+                    } else if (s.length() > 10) {
                         System.out.println(s);
-                    }   else {
-                          System.out.println(s);
+                    } else {
+                        System.out.println(s);
                     }
                 });
     }
