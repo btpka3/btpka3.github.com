3a18c8364e6adb56ad13602c9c257a812a5de322
btpka3 <btpka3@163.com>
2025-12-26 19:48:56 +0800

    update

--- java/jdk/TestJDK/hello/src/test/java/me/test/jdk/java/text/MessageFormat01.java
+++ java/jdk/TestJDK/hello/src/test/java/me/test/jdk/java/text/MessageFormat01.java
@@ -1,36 +1,79 @@
 package me.test.jdk.java.text;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 
 import java.text.MessageFormat;
-import java.text.NumberFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
+import static java.util.Locale.CHINESE;
+import static java.util.Locale.ENGLISH;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class MessageFormat01 {
 
+
+//    private Map<Locale, List<Object>> map = Map.of(
+//            Locale.CHINESE, List.of(
+//                    List.of("{0}", new Object[]{1234567890.123456}, "1,234,567,890.123"),
+//
+//            )
+//    );
+
+    @Data
+    @AllArgsConstructor
+    static class Rec {
+        String pattern;
+        Object[] args;
+        /**
+         * KEY=目标 locale
+         * VALUE=期待的结果
+         */
+        Map<Locale, String> expected;
+    }
+
+    List<Rec> recList = List.of(
+            new Rec("{0}", new Object[]{1234567890.123456}, Map.of(Locale.CHINESE, "1,234,567,890.123", Locale.ENGLISH, "1,234,567,890.123"))
+    );
+
+
     @Test
     public void test() {
-        Locale.setDefault(Locale.CHINESE);
-        assertSame(Locale.CHINESE, Locale.getDefault(Locale.Category.FORMAT));
-        assertSame(Locale.CHINESE, Locale.getDefault());
+//        Locale targetLocale = Locale.CHINESE;
+        Locale targetLocale = Locale.ENGLISH;
+        Locale.setDefault(targetLocale);
+        assertSame(targetLocale, Locale.getDefault(Locale.Category.FORMAT));
+        assertSame(targetLocale, Locale.getDefault());
+
+        {
+            String msg = MessageFormat.format("{0}", 1234567890.123456);
+            assertEquals("1,234,567,890.123", msg);
+        }
+
         {
             String msg = MessageFormat.format("{0,number,percent}", 1.23456);
             assertEquals("123%", msg);
         }
         // \u00A4 : `¤` 表示货币
         {
-            String msg = MessageFormat.format("{0,number,currency}", 1234567890);
-            assertEquals("¤1,234,567,890.00", msg);
+            String msg = MessageFormat.format("{0,number,currency}", 1234567890.123);
+            assertEquals("¤1,234,567,890.12", msg);
         }
         {
-            String msg = MessageFormat.format("{0,number,¤#}", 1234567890);
-            assertEquals("¤1234567890", msg);
+            String msg = MessageFormat.format("{0,number,¤#.##}", 1234567890.123);
+            assertEquals("¤1234567890.12", msg);
         }
+        {
+            String msg = MessageFormat.format("{0,number,¤¤#.##}", 1234567890.123);
+            assertEquals("XXX1234567890.12", msg);
+        }
+
 
         // 注意：默认数值还是会有逗号分隔的
         {
@@ -68,6 +111,120 @@ public class MessageFormat01 {
             assertEquals("12345.10", msg);
         }
 
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0}", date);
+
+            if (CHINESE == targetLocale) {
+                assertEquals("2001/1/2 03:04", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("1/2/01, 3:04 AM", msg);
+            }
+
+        }
+
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,date}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("2001年1月2日", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("Jan 2, 2001", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,date,short}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("2001/1/2", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("1/2/01", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,date,medium}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("2001年1月2日", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("Jan 2, 2001", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,date,long}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("2001年1月2日", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("January 2, 2001", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,date,full}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("2001年1月2日星期二", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("Tuesday, January 2, 2001", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,time}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("03:04:05", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("3:04:05 AM", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,time,short}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("03:04", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("3:04 AM", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,time,medium}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("03:04:05", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("3:04:05 AM", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,time,long}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("CST 03:04:05", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("3:04:05 AM CST", msg);
+            }
+        }
+        {
+            Date date = new Date(978375845789L);
+            String msg = MessageFormat.format("{0,time,full}", date);
+            if (CHINESE == targetLocale) {
+                assertEquals("中国标准时间 03:04:05", msg);
+            }
+            if (ENGLISH == targetLocale) {
+                assertEquals("3:04:05 AM China Standard Time", msg);
+            }
+        }
+
         // "2001-01-02 03:04:05.789" == 978375845789
         // 参考 SimpleDateFormat, 注意：最后的一个大S数量多少均不影响结果
         {
@@ -89,4 +246,5 @@ public class MessageFormat01 {
             assertEquals("{0},'bbb',ccc", msg);
         }
     }
+
 }
