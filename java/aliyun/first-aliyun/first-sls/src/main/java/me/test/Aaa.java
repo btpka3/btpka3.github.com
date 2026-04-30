75a576382e91d95ea4f4515cb2a5144b5eb88a03
btpka3 <btpka3@163.com>
2026-04-30 10:45:59 +0800

    update

---
+++ java/aliyun/first-aliyun/first-sls/src/main/java/me/test/Aaa.java
@@ -0,0 +1,13 @@
+package me.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aaa {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aaa.class, args);
+	}
+
+}
