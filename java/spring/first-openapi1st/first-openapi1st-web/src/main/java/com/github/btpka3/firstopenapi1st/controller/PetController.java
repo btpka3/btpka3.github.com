c08bdb4fd77358131903b1e3e89dfc1e6e8e33b3
btpka3 <btpka3@163.com>
2026-05-29 16:53:47 +0800

    由既有的 openapi json/yaml 生成http api 接口，再去实现。

--- java/spring/first-openapi1st/first-openapi1st-web/src/main/java/com/github/btpka3/firstopenapi1st/controller/PetController.java
+++ java/spring/first-openapi1st/first-openapi1st-web/src/main/java/com/github/btpka3/firstopenapi1st/controller/PetController.java
@@ -2,7 +2,8 @@ package com.github.btpka3.firstopenapi1st.controller;
 
 import com.github.btpka3.firstopenapi1st.api.PetApi;
 import com.github.btpka3.firstopenapi1st.model.Pet;
-import org.springframework.http.ResponseEntity;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,21 +11,24 @@ import java.util.List;
 @RestController
 public class PetController implements PetApi {
 
+    @Autowired
+    private HttpServletResponse response;
+
     @Override
-    public ResponseEntity<Pet> getPetById(Long petId) {
+    public Pet getPetById(Long petId) {
         Pet pet = new Pet();
         pet.setId(petId);
         pet.setName("doggie");
         pet.setStatus(Pet.StatusEnum.AVAILABLE);
-        return ResponseEntity.ok(pet);
+        return pet;
     }
 
     @Override
-    public ResponseEntity<List<Pet>> findPetsByStatus(List<String> status) {
+    public List<Pet> findPetsByStatus(List<String> status) {
         Pet pet = new Pet();
         pet.setId(1L);
         pet.setName("doggie");
         pet.setStatus(Pet.StatusEnum.AVAILABLE);
-        return ResponseEntity.ok(List.of(pet));
+        return List.of(pet);
     }
 }
